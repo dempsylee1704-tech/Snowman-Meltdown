3c1f6acc2a9d418bf9f921f0f6530fea668cd926
German <dempsylee1704@gmail.com>
2025-12-05 13:13:03 +0100

    Included a replay feature

--- snowman.py
+++ snowman.py
@@ -2,4 +2,10 @@ from game_logic import play_game
 
 
 if __name__ == "__main__":
-    play_game()
+    while True:
+        play_game()
+
+        replay_game = input("Play again? (y/n):")
+        if replay_game == "n":
+            print("Thank you for playing!")
+            break
